26fd1efb97cf4d9a340c511a2d366479397bd309
letriduc116 <letriduc11062004@gmail.com>
2024-11-03 21:37:36 +0700

    complete product list in product page

---
+++ Homepage/assets/js/product.js
@@ -0,0 +1,27 @@
+const products = document.querySelectorAll('.box__bike--item');
+let counter = 0; 
+
+function left () {
+    if(counter == 0 ) {
+        counter = products.length/3 - 1;
+    } else {
+        counter--;
+    }
+    scroll();
+}
+
+function right () {
+
+    if(counter == products.length/3 - 1) {
+        counter = 0;
+    } else {
+        counter++;
+    }
+    scroll();
+}
+
+function scroll() {
+    products.forEach(function(item){
+        item.style.transform = `translateX(-${counter * 100}%)`;
+    })
+}
