f5751554604218725f5232b1e1c15f6a63209d9e
letriduc116 <letriduc11062004@gmail.com>
2024-11-06 22:48:06 +0700

    update pagination product

--- Homepage/assets/js/product.js
+++ Homepage/assets/js/product.js
@@ -1,4 +1,9 @@
-const products = document.querySelectorAll('.box__bike--item');
+const $ = document.querySelector.bind(document);
+const $$ = document.querySelectorAll.bind(document);
+
+
+// slider for product
+const products = $$('.box__bike--item');
 let counter = 0; 
 
 function left () {
@@ -24,3 +29,35 @@ function scroll() {
         item.style.transform = `translateX(-${counter * 100}%)`;
     })
 }
+
+// pagination for product
+const paginationLink = document.getElementsByClassName('pagination__link');
+let currentPage = 1;
+
+function changePage () {
+    for(link of paginationLink) {
+        link.classList.remove('pagination__link--active');
+    }
+    event.target.classList.add('pagination__link--active');
+    currentPage = event.target.value;
+
+}
+
+function backBtn () {
+    if(currentPage == 1) {
+        currentPage = paginationLink.length;
+    } else {
+        currentPage--;
+    }
+    paginationLink[currentPage - 1].click();
+}
+
+function nextBtn () {
+    if(currentPage == paginationLink.length) {
+        currentPage = 1;
+    } else {
+        currentPage++;
+    }
+    paginationLink[currentPage - 1].click();
+}
+
