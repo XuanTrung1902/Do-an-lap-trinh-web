244ae579554b865146cd70aea830a3ae78b58f53
vadolaxt <22130304@st.hcmuaf.edu.vn>
2024-11-09 01:03:34 +0700

    sua trang thanh toan

--- Web/assets/js/Chi tiet san pham.js
+++ Web/assets/js/Chi tiet san pham.js
@@ -1,13 +1,3 @@
-// hiện ra các loại thông số trong thông số kỹ thuật ra chi tiết
-function toggleSubOptions(id, element) {
-    var subOptions = document.getElementById(id);
-    if (subOptions.style.display === "none" || subOptions.style.display === "") {
-        subOptions.style.display = "block";
-    } else {
-        subOptions.style.display = "none";
-    }
-}
-
 // thay doi mau xe dua theo lua chon
 function changeBikeColor(id) {
     var imgscr = document.getElementById("img");
