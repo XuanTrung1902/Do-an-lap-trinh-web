43acf5cdc2dccd0e1b3b542915648d893be07643
vadolaxt <22130304@st.hcmuaf.edu.vn>
2024-11-13 16:04:13 +0700

    trang kiem tra thong tin thnah toan

--- Web/assets/js/Thanh toan.js
+++ Web/assets/js/Thanh toan.js
@@ -5,18 +5,17 @@ function showAddressSection() {
     const addressDiv = document.getElementById("address");
     const deposit = document.getElementById("deposit");
     const shipping = document.getElementById("shipping");
-
-
+    const shop = document.getElementById("shop-address");
 
     if (option1.checked) {
         addressDiv.style.display = "flex";
-        // shipping.style.display = "flex";
-        // deposit.style.display = "none";
-
+        shipping.style.display = "block";
+        deposit.style.display = "none";
+        shop.style.display = "none";
     } else {
         addressDiv.style.display = "none";
-        // deposit.style.display = "flex";
-        // shipping.style.display = "none";
-
+        deposit.style.display = "block";
+        shipping.style.display = "none";
+        shop.style.display = "block";
     }
 }
