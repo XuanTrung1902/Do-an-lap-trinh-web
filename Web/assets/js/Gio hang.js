fb234a87c3b7c228d7dee4604bc4320b150ca5c0
vadolaxt <22130304@st.hcmuaf.edu.vn>
2024-11-27 23:58:39 +0700

    js gio hang

--- Web/assets/js/Gio hang.js
+++ Web/assets/js/Gio hang.js
@@ -5,7 +5,7 @@ function deleteProduct(list ,id) {
     const toastLiveExample = document.getElementById('liveToast');
 
     product.style.display = "none";
-    console.log("Xóa sản phẩm với ID:", listProduct);
+    // console.log("Xóa sản phẩm với ID:", listProduct);
     if (product) {
         const toast = bootstrap.Toast.getOrCreateInstance(toastLiveExample);
         // const toast = new bootstrap.Toast(toastLiveExample);
@@ -13,5 +13,41 @@ function deleteProduct(list ,id) {
             toast.show()
         })
     }
+}
+
+function cartItemChecked(){
+    const cart = document.getElementById('cart')
+    const brands = document.getElementsByClassName("brand")
+    const items = document.getElementsByClassName("item")
+    cart.addEventListener('change', () => {
+        const isChecked = cart.checked; 
+        for (let i = 0; i < brands.length; i++) {
+            brands[i].checked = isChecked; 
+        }
+        for (let i = 0; i < items.length; i++) {
+            items[i].checked = isChecked; 
+        }
+    });
+}
+function brandItemChecked(idBrand, idItem){
+    const brand = document.getElementById(idBrand)
+    const item = document.getElementsByClassName(idItem)
+    const isChecked = brand.checked
+    for (let i = 0; i < item.length; i++) {
+        item[i].checked = isChecked;
+    }
+}
+function increase(idText) {
+    const quantity = document.getElementById(idText);
+    let currentQuantity = parseInt(quantity.textContent);
+    currentQuantity += 1;
+    quantity.textContent = currentQuantity;
+}
+
+function decrease(idText) {
+    const quantity = document.getElementById(idText);
+    let currentQuantity = parseInt(quantity.textContent);
+    currentQuantity = Math.max(1, currentQuantity - 1);
+    quantity.textContent = currentQuantity;
 }
 
