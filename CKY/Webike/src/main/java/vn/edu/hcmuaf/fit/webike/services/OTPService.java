46edee6aaa16805f9ca1903a8d0f718a816f0a4b
tan22130248 <22130248@st.hcmuaf.edu.vn>
2025-03-09 14:34:26 +0700

    clear nhanh

--- CKY/Webike/src/main/java/vn/edu/hcmuaf/fit/webike/services/OTPService.java
+++
@@ -1,9 +0,0 @@
-package vn.edu.hcmuaf.fit.webike.services;
-import java.util.Random;
-public class OTPService {
-    public static String generateOTP() {
-        Random random = new Random();
-        int otp = 100000 + random.nextInt(900000);
-        return String.valueOf(otp);
-    }
-}
