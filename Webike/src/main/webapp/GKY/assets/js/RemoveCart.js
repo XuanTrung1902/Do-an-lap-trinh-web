0d8ec0e13e69fa59ff7a21f23deffd115cb11551
Xuantrung1902 <22130304@st.hcmuaf.edu.vn>
2025-03-22 00:30:33 +0700

    hoan thanh ajax xoa sp khoi cart #33

---
+++ Webike/src/main/webapp/GKY/assets/js/RemoveCart.js
@@ -0,0 +1,19 @@
+function remove(itemId) {
+    fetch(`/Webike/remove-cart`, {
+        method: "POST",
+        headers: {"Content-Type": "application/json"},
+        body: JSON.stringify({id: itemId})
+    })
+        .then(response => response.json())
+        .then(data => {
+            if (data.isSuccess) {
+                return fetch(`/Webike/show-cart`);
+            }
+        })
+        .then(response => response.text())
+        .then(html => {
+            document.getElementById("cart").innerHTML = html;
+        })
+        .catch(error => console.error(`Lỗi: ${error}`));
+}
+
