da84522a34ff49c37c76975e09e2154c9372cd0f
Xuantrung1902 <22130304@st.hcmuaf.edu.vn>
2025-04-11 23:14:08 +0700

    chinh sua PR #67 sau khi dc rv

--- Webike/src/main/java/vn/edu/hcmuaf/fit/webike/controllers/payment/Pay.java
+++ Webike/src/main/java/vn/edu/hcmuaf/fit/webike/controllers/payment/Pay.java
@@ -17,9 +17,9 @@ import java.util.StringTokenizer;
 @WebServlet(name = "Pay", value = "/pay")
 public class Pay extends HttpServlet {
 
+
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-    }
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
@@ -51,7 +51,7 @@ public class Pay extends HttpServlet {
             int pid = Integer.parseInt(o.getProductID());
             String img = o.getImg();
             Product p = productDAO.getProduct(pid);
-            int productQuantity= p.getQuantity() - quantity;
+            int productQuantity = p.getQuantity() - quantity;
             int updateProductQuantity = productDAO.updateQuantity(pid, productQuantity);
             int insertOrderItem = dao.insertOrderItem(quantity, img, color, oid, pid);
         }
@@ -61,4 +61,57 @@ public class Pay extends HttpServlet {
         request.getSession().removeAttribute("order");
 
     }
+//
+//    @Override
+//    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+//
+//        PaymentDAO dao = new PaymentDAO();
+//        Order order = (Order) request.getSession().getAttribute("order");
+//
+////        double deposit = Double.parseDouble(request.getParameter("deposit"));
+////        double remain = Double.parseDouble(request.getParameter("remain"));
+////        String appointment = request.getParameter("appointment");
+////        String address = request.getParameter("address");
+////        String status = "Đã cọc";
+////        int accountID = Integer.parseInt(request.getParameter("accountID"));
+////
+////        // branch là chi nhánh =))
+////        String branch = request.getParameter("branch");
+////        StringTokenizer t = new StringTokenizer(branch, "-");
+////        int shopID = Integer.parseInt(t.nextToken());
+//
+//        int oid = Integer.parseInt(request.getParameter("vnp_TxnRef"));
+//        double deposit = (double) request.getAttribute("vnp_Amount");
+//        double remain = (double) request.getSession().getAttribute("remain");
+//        String appointment = (String) request.getSession().getAttribute("appointment");
+//        String payDate = request.getParameter("vnp_CreateDate");
+//        String address = (String) request.getSession().getAttribute("address");
+//        String status = request.getParameter("vnp_ResponseCode");
+//        int accountID = (int) request.getSession().getAttribute("accountID");
+//        int shopID= (int) request.getSession().getAttribute("shopID");
+//
+//
+//        int insertOrder = dao.insertOrder(oid, deposit, remain, address, appointment, null, status, accountID, shopID);
+//        System.out.println("Order id:" + oid);
+//        System.out.println("Order insert:" + insertOrder);
+//        System.out.println("Pay date: " + payDate);
+//
+//        request.getSession().setAttribute("oid", oid);
+//        request.getSession().setAttribute("deposit", deposit);
+//
+//
+////        for (OrderItem o : order.getData()) {
+////            ProductDAO productDAO = new ProductDAO();
+////            int quantity = o.getQuantity();
+////            String color = o.getColor();
+////            int pid = Integer.parseInt(o.getProductID());
+////            String img = o.getImg();
+////            Product p = productDAO.getProduct(pid);
+////            int productQuantity= p.getQuantity() - quantity;
+////            int updateProductQuantity = productDAO.updateQuantity(pid, productQuantity);
+////            int insertOrderItem = dao.insertOrderItem(quantity, img, color, oid, pid);
+////        }
+//        response.sendRedirect("/Webike/GKY/billing.jsp");
+//        request.getSession().removeAttribute("order");
+//    }
 }
