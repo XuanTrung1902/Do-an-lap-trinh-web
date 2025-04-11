1ef2a58a89e792967fd2bc26ec77d70f5115bd83
Xuantrung1902 <22130304@st.hcmuaf.edu.vn>
2025-04-11 13:09:45 +0700

    Revert "tich hop vnpay"
    This reverts commit 4e660a537956b015aa8c4f9bbf676fb2795a2ab7, reversing
    changes made to 45010783ff1039648f14ec464f2496031cfb18df.

--- Webike/src/main/java/vn/edu/hcmuaf/fit/webike/dao/PaymentDAO.java
+++ Webike/src/main/java/vn/edu/hcmuaf/fit/webike/dao/PaymentDAO.java
@@ -11,7 +11,6 @@ public class PaymentDAO {
         PaymentDAO dao = new PaymentDAO();
 //        System.out.println(dao.getShops());
 //        dao.insertOrder(12.9,12.8, "Address", "2020-02-01", "2020-02-01","Status", 1,1);
-//        System.out.println(dao.creatOrderID());
     }
 
     public List<Shop> getShops() {
@@ -20,36 +19,29 @@ public class PaymentDAO {
         return jdbi.withHandle(handle -> handle.createQuery(sql).mapToBean(Shop.class).list());
     }
 
-    public int creatOrderID() {
-        Jdbi jdbi = JDBIConnect.get();
-        String sql = "SELECT max(id) from orders";
-        int rs = jdbi.withHandle(handle -> handle.createQuery(sql)
-                .mapTo(Integer.class)
-                .one() + 1);
-        return rs;
-    }
-
     public int insertOrder(double deposit, double remain, String address, String appointment, String payDate, String status, int accountID, int shopID) {
         Jdbi jdbi = JDBIConnect.get();
         String sql = """
                 insert into orders (deposit, remain, address, appointment, payDate, status, accountID, shopID)
-                values (:deposit, :remain, :address, :appointment, :payDate, :status, :accountID, :shopID)
+                values (:deposit, :remain, :address, :appointment, :payDate, :status, :accountID, :shopID) 
                 """;
         return jdbi.withHandle(handle -> handle.createUpdate(sql)
-                        .bind("deposit", deposit)
-                        .bind("remain", remain)
-                        .bind("address", address)
-                        .bind("appointment", appointment)
-                        .bind("payDate", payDate)
-                        .bind("status", status)
-                        .bind("accountID", accountID)
-                        .bind("shopID", shopID)
-                        .executeAndReturnGeneratedKeys()
-                        .mapTo(int.class)
-                        .first()
-//                        .execute() > 0 ? 1 : 0
+                .bind("deposit", deposit)
+                .bind("remain", remain)
+                .bind("address", address)
+                .bind("appointment", appointment)
+                .bind("payDate", payDate)
+                .bind("status", status)
+                .bind("accountID", accountID)
+                .bind("shopID", shopID)
+                .executeAndReturnGeneratedKeys()
+                .mapTo(int.class)
+                .first()
         );
     }
+
+
+
 
 
     public int insertOrderItem(int quantity, String img, String color, int oid, int pid) {
