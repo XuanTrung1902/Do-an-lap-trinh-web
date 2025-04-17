6ec193dc0b1f1153aa0964c2936c1df22522f946
Xuantrung1902 <22130304@st.hcmuaf.edu.vn>
2025-04-17 12:44:32 +0700

    chỉnh sửa cmt ở productDetail #36

--- Webike/src/main/webapp/GKY/assets/js/productDetail.js
+++ Webike/src/main/webapp/GKY/assets/js/productDetail.js
@@ -5,12 +5,30 @@ function changeBikeColor(id) {
     if (btnid.id == "red") {
         imgscr.src = "assets/img/ducati red.png";
         btnid.style.background = "rgb(147, 157, 163)";
-        document.getElementById("black").style.background ="none"
-    } else if(btnid.id){
+        document.getElementById("black").style.background = "none"
+    } else if (btnid.id) {
         imgscr.src = "assets/img/ducati black.png";
         btnid.style.background = "rgb(147, 157, 163)";
-        document.getElementById("red").style.background ="none"
+        document.getElementById("red").style.background = "none"
     }
 }
 
+function changeBikeColor(id) {
+    fetch(`/Webike/remove-cart`, {
+        method: "POST",
+        headers: {"Content-Type": "application/json"},
+        body: JSON.stringify({id: itemId})
+    })
+        .then(response => response.json())
+        .then(data => {
+            if (data.isSuccess) {
+                return fetch(`/Webike/show-cart`);
+            }
+        })
+        .then(response => response.text())
+        .then(html => {
+            document.getElementById("main").innerHTML = html;
+        })
+        .catch(error => console.error(`Lỗi: ${error}`));
+}
 
