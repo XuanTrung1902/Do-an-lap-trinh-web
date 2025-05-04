7e47302249a3224564130911854f2c3a7dc2966e
Nguyễn Quốc Tấn <22130248@st.hcmuaf.edu.vn>
2025-05-04 16:46:26 +0700

    xóa các phần test

--- Webike/src/main/java/vn/edu/hcmuaf/fit/webike/controllers/users/Enable2FAController.java
+++ Webike/src/main/java/vn/edu/hcmuaf/fit/webike/controllers/users/Enable2FAController.java
@@ -16,49 +16,6 @@ import java.net.URLEncoder;
 @WebServlet(name = "Enable2FAController", value = "/enable-2fa")
 public class Enable2FAController extends HttpServlet {
 
-//    @Override
-//    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-//        HttpSession session = req.getSession();
-//        User user = (User) session.getAttribute("auth");
-//        System.out.println(user.toString());
-//
-//        if (user.getOtpSecret() == null || user.getOtpSecret().isEmpty()) {
-//            GoogleAuthenticator gAuth = new GoogleAuthenticator();
-//            final GoogleAuthenticatorKey key = gAuth.createCredentials();
-//            String secret = key.getKey();
-//
-//            // Lưu vào DB
-//            user.setOtpSecret(secret);
-//            user.setOtpEnabled(false); // Chưa xác thực, chỉ mới tạo QR
-//            new UserDao().updateOtpSecret(user.getId(), secret);
-//
-//            // Gán lại session để lưu secret tạm thời (nếu cần)
-//            session.setAttribute("auth", user);
-//        }
-//
-////        String otpSecret = user.getOtpSecret();
-////        GoogleAuthenticatorKey key = new GoogleAuthenticatorKey.Builder(otpSecret).build();
-////        String otpAuthURL = GoogleAuthenticatorQRGenerator.getOtpAuthURL("Webike", user.getPhoneNum(), key);
-////        System.out.println(otpAuthURL);
-////        req.setAttribute("qrUrl", otpAuthURL);
-////        req.getRequestDispatcher("/GKY/trangTTKhachHang.jsp").forward(req, resp);
-//        String otpSecret = user.getOtpSecret();
-//        if (otpSecret != null && !otpSecret.isEmpty()) {
-//            String otpAuthURL = "otpauth://totp/Webike:" + user.getEmail()
-//                    + "?secret=" + otpSecret + "&issuer=Webike";
-//            System.out.println("OTP URL: " + otpAuthURL);
-//
-//            String encodedUrl = URLEncoder.encode(otpAuthURL, "UTF-8");
-//            resp.setContentType("application/json");
-//            PrintWriter out = resp.getWriter();
-//            out.print("{\"qrUrl\": \"" + otpAuthURL + "\"}");
-//            out.flush();
-//        } else {
-//            System.out.println("otpSecret null hoặc rỗng!");
-//            // Chuyển hướng hoặc thông báo lỗi cho user
-//        }
-//
-//    }
 @Override
 protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
     HttpSession session = req.getSession(false);
@@ -98,30 +55,6 @@ protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws Se
 
     out.flush();
 }
-//
-//    @Override
-//    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-//        HttpSession session = req.getSession();
-//        User user = (User) session.getAttribute("auth");
-//        String otp = req.getParameter("otp");
-//
-//        GoogleAuthenticator gAuth = new GoogleAuthenticator();
-//        boolean isCodeValid = gAuth.authorize(user.getOtpSecret(), Integer.parseInt(otp));
-//
-//        if (isCodeValid) {
-//            user.setOtpEnabled(true);
-//            new UserDao().updateOtpEnabled(user.getId(), true);
-//            session.setAttribute("auth", user);
-//            req.setAttribute("message", "Đã bật xác thực 2 bước thành công!");
-//        } else {
-//            req.setAttribute("error", "Mã OTP không chính xác!");
-//        }
-////        req.getRequestDispatcher("Profile").forward(req, resp);
-////        resp.sendRedirect("/Profile"); // Chuyển hướng đến trang đăng nhập
-//
-//        req.getRequestDispatcher("/GKY/trangTTKhachHang.jsp").forward(req, resp);
-//    }
-
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         HttpSession session = req.getSession(false);
