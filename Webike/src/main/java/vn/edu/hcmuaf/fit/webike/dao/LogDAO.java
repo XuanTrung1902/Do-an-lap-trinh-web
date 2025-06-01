5568b1397816d9fa16f0d5c38b5f3a6e4786db91
Nguyễn Quốc Tấn <22130248@st.hcmuaf.edu.vn>
2025-06-01 18:09:41 +0700

    Sử lý servlet lấy log #149

--- Webike/src/main/java/vn/edu/hcmuaf/fit/webike/dao/LogDAO.java
+++ Webike/src/main/java/vn/edu/hcmuaf/fit/webike/dao/LogDAO.java
@@ -1,8 +1,10 @@
 package vn.edu.hcmuaf.fit.webike.dao;
 
 import vn.edu.hcmuaf.fit.webike.db.JDBIConnect;
+import vn.edu.hcmuaf.fit.webike.models.Log;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 public class LogDAO {
     public void insertLog(String level, LocalDateTime logTime, String location, String userInfo, String before, String after) {
@@ -15,6 +17,14 @@ public class LogDAO {
                         .bind("before", before)
                         .bind("after", after)
                         .execute()
+        );
+    }
+    // Lấy danh sách toàn bộ logs (sắp xếp theo thời gian mới nhất)
+    public List<Log> getAllLogs() {
+        return JDBIConnect.get().withHandle(handle ->
+                handle.createQuery("SELECT * FROM logs ORDER BY logTime DESC")
+                        .mapToBean(Log.class)
+                        .list()
         );
     }
 }
